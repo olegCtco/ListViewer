9fa83b528e9c961a5fc01d7a72835a60e1519124
olegCtco <olegs.fedkins@gmail.com>
2013-08-17 16:25:01 +0300

    push test removed line

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,5 @@ import lv.ctco.ListViewerKVO.ListViewer;
 public class Main {
     public static void main(String[] args) {
         new ListViewer().start();
-        System.out.println("test push");
     }
 }
