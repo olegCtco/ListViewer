23709b01b903de43dda3b813f92860ccea24c8c6
olegCtco <olegs.fedkins@gmail.com>
2013-08-17 16:23:46 +0300

    push test

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,6 @@ import lv.ctco.ListViewerKVO.ListViewer;
 public class Main {
     public static void main(String[] args) {
         new ListViewer().start();
+        System.out.println("test push");
     }
 }
