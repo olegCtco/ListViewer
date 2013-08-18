e738f207100ebba141e0e502ee197e693cee57d0
olegCtco <olegs.fedkins@gmail.com>
2013-08-18 11:02:43 +0300

    public to private studentList

--- src/main/java/lv/ctco/ListViewerKVO/StudentList.java
+++ src/main/java/lv/ctco/ListViewerKVO/StudentList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentList {
-    public static List<Student> studentList = new ArrayList<>();
+    private static List<Student> studentList = new ArrayList<>();
 
     public static List<Student> getStudentList() {
         return studentList;
