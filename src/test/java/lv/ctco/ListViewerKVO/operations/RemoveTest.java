0ed1ec3e2d6309dfacef1fe8f37fbfd375079211
olegCtco <olegs.fedkins@gmail.com>
2013-08-18 13:45:35 +0300

    all tests added

--- src/test/java/lv/ctco/ListViewerKVO/operations/RemoveTest.java
+++ src/test/java/lv/ctco/ListViewerKVO/operations/RemoveTest.java
@@ -1,7 +1,12 @@
 package lv.ctco.ListViewerKVO.operations;
 
 import lv.ctco.ListViewerKVO.Operation;
+import lv.ctco.ListViewerKVO.Student;
+import lv.ctco.ListViewerKVO.StudentsList;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,7 +17,49 @@ public class RemoveTest {
     }
 
     @Test
-    public void testDoOperation() {
+    public void testDoOperationBeforeRange() {
+        String operation = Operation.REMOVE.name();
+        String index = "0";
+        String name = "Janis";
+        String surname = "Berzins";
+        String university = "RTU";
+        List<String> stringList = new ArrayList<>();
+        List<Student> studentList = StudentsList.getStudentList();
+        stringList.add(operation);
+        stringList.add(index);
+        studentList.add(new Student(name, surname, university));
+        assertEquals(new Remove().doOperation(stringList), studentList);
+    }
 
+    @Test
+    public void testDoOperationInRange() {
+        String operation = Operation.REMOVE.name();
+        String index = "1";
+        String name = "Janis";
+        String surname = "Berzins";
+        String university = "RTU";
+        List<String> stringList = new ArrayList<>();
+        List<Student> studentList = StudentsList.getStudentList();
+        stringList.add(operation);
+        stringList.add(index);
+        studentList.clear();
+        studentList.add(new Student(name, surname, university));
+        assertEquals(new Remove().doOperation(stringList), new ArrayList<Student>());
+    }
+
+    @Test
+    public void testDoOperationAfterRange() {
+        String operation = Operation.REMOVE.name();
+        String index = "2";
+        String name = "Janis";
+        String surname = "Berzins";
+        String university = "RTU";
+        List<String> stringList = new ArrayList<>();
+        List<Student> studentList = StudentsList.getStudentList();
+        stringList.add(operation);
+        stringList.add(index);
+        studentList.clear();
+        studentList.add(new Student(name, surname, university));
+        assertEquals(new Remove().doOperation(stringList), studentList);
     }
 }
