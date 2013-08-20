bd0824b811a2f08686cded4e08781ca9ff3bf4b8
VadiButross <GitHubForCtco123>
2013-08-20 12:00:56 +0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ import lv.ctco.ListViewerKVO.ListViewer;
 public class Main {
     public static void main(String[] args) {
         new ListViewer().start();
+        System.out.println("hi");
     }
 }
